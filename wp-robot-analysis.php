5c660f15996ad9ec6e69049f44a319c1da91d2e3
macctown <macctown@hotmail.com>
2015-02-16 20:06:15 -0500

    initial basic info
    initial basic info

---
+++ wp-robot-analysis.php
@@ -0,0 +1,27 @@
+<?php
+/*
+Plugin Name: wp-robot-analysis
+Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+Description: Strong Robot Analysis Tool For WP
+Version: 1.0
+Author: Wei Zhang
+Author URI: http://blog.zhang-wei.me
+*/
+
+/*  Copyright 2015  Wei Zhang  (email : macctown@hotmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+?>
